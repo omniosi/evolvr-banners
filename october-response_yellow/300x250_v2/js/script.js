728d51eb85aaa1d955e00488059e253a8403e2c0
ornette coleman <omniosi@gmail.com>
2016-02-24 12:36:20 -0800

    finalizing yellow animation. fixed scrollbar issue on both.

--- october-response_yellow/300x250_v2/js/script.js
+++ october-response_yellow/300x250_v2/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $("#f5_btn img").hover(function() {
-        $(this).attr("src","../img/f5_btn_hover.png");
+        $(this).attr("src","img/f5_btn_hover.png");
             }, function() {
-        $(this).attr("src","../img/f5_btn.png");
+        $(this).attr("src","img/f5_btn.png");
     });
 });
 
@@ -19,14 +19,23 @@ var f1_t1 = document.getElementById("f1_txt1")
 ,     f3_i1 = document.getElementById("f3_img1")
 ,     f3_i2 = document.getElementById("white_patch")
 ,     f3_i3 = document.getElementById("cross_sign")
+,     f4_t1 = document.getElementById("f4_txt1")
+,     f4_t2 = document.getElementById("f4_txt2")
+,     f4_i1 = document.getElementById("f4_img1")
+,     f4_i2 = document.getElementById("green_patch")
+,     f4_i3 = document.getElementById("epipen_pack")
+,     f5_i1 = document.getElementById("f5_pen")
+,     f5_i2 = document.getElementById("logo")
+,     f5_i3 = document.getElementById("f5_btn")
+,     f5_shine = document.getElementById("shine")
 ;
 
 // Animation
 var t1 = new TimelineLite();
 
 t1
-.from( f1_t1, 1, {left: 300, ease:"easeInOutBack"})
-.from( f1_t2, 1, {left: 300, ease:"easeInOutBack"},"-=0.5")
+// .from( f1_t1, 0.5, {left: 300, ease:"easeOutExpo"})
+.from( f1_t2, 1, {left: 300, ease:"easeOutExpo"},"+=0.5")
 .from( f1_i1, 0.5, {scale: 0, ease:"easeOutBack"})
 .to( f2_i1, 1, {scale:0.5,left:120,top:-58, ease:"easeInOutBack"})
 .to( f1_t2, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"+=1")
@@ -34,6 +43,35 @@ t1
 .to( f2_t1, 1, {autoAlpha:1, ease:"easeInOutBack"},"-=1")
 .to( f2_i1, 1, {scale:1,left:175,top:26, ease:"easeInOutBack"},"-=1")
 .to( f1_i1, 1, {scale:0.5,left:270,top:80, ease:"easeInOutBack"},"-=1")
-.to( f2_i2, 1, {opacity:1, ease:"easeInOutBack"})
+// .to( f2_i2, 1, {opacity:1, ease:"easeInOutBack"})
 .to( f3_i1, 1, {scale:0.5,left:120,top:-58, ease:"easeInOutBack"})
+.to( f2_t1, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"+=1")
+.to( f1_t1, 1, {top: "30", ease:"easeInOutBack"})
+.from( f3_t1, 1, {top: "-=20", ease:"easeInOutBack"},"-=0.5")
+.to( f3_t1, 1, {autoAlpha:1, ease:"easeInOutBack"},"-=1")
+.to( f3_i1, 1, {scale:1,left:175,top:26, ease:"easeInOutBack"},"-=1")
+.to( f2_i1, 1, {scale:0.5,left:247,top:88, ease:"easeInOutBack"},"-=1")
+.to( f1_i1, 1, {left:320,top:120,autoAlpha:0, ease:"easeInOutBack"},"-=1")
+.to( f4_i1, 1, {scale:0.5,left:120,top:-58, ease:"easeInOutBack"},"four")
+.to( f4_i1, 1, {opacity:1, ease:"easeInOutBack"},"-=1")
+.to( f1_t1, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"+=1")
+.to( f3_t1, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"-=0.5")
+.from( f4_t1, 1, {top: "-=20", ease:"easeInOutBack"})
+.to( f4_t1, 1, {opacity:1, ease:"easeInOutBack"},"-=1")
+.from( f4_t2, 1, {top: "-=20", ease:"easeInOutBack"},"-=0.5")
+.to( f4_t2, 1, {opacity:1, ease:"easeInOutBack"},"-=1")
+.to( f4_i1, 1, {scale:1,left:185,top:33, ease:"easeInOutBack"},"-=2")
+.to( f3_i1, 1, {scale:0.5,left:300,top:88,autoAlpha:0, ease:"easeInOutBack"},"-=2")
+.to( f2_i1, 1, {left:300,top:100,autoAlpha:0, ease:"easeInOutBack"},"-=2")
+.to( f4_i1, 1, {rotation:47,top:-7,left:197, ease:"easeInOutBack"},"+=1")
+.to( f5_i1, 1, {autoAlpha:1, ease:"easeInOutBack"},"-=0.5")
+.to( f5_i1, 0.5, {scale:1,top:10,left:10, ease:"easeInOut"})
+.to( f4_i1, 0.5, {autoAlpha:0, ease:"easeInOutBack"},"-=1")
+.to( f4_t1, 0.5, {autoAlpha:0, ease:"easeInOutBack"},"-=1.5")
+.to( f4_t2, 0.5, {autoAlpha:0, ease:"easeInOutBack"},"-=1.5")
+.to( f5_i2, 0.5, {autoAlpha:1})
+.to( f5_i3, 0.5, {autoAlpha:1})
+.from( f5_i2, 1, {left:300, ease:"easeInOutBack"},"-=2")
+.from( f5_i3, 0.5, {left:300, ease:"easeInOutExpo"},"-=0.5")
+// .from( f5_i3, 0.5, {left:300, ease:Back.easeInOut.config(0.5)},"-=0.5")
 ;
