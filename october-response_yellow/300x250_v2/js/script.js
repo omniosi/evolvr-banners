6332610177500c54a09c766069e22351e80fee10
ornette coleman <omniosi@gmail.com>
2016-02-26 16:27:16 -0800

    re-setup for client updates

--- october-response_yellow/300x250_v2/js/script.js
+++ october-response_yellow/300x250_v2/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     $("#f5_btn img").hover(function() {
         $(this).attr("src","img/f5_btn_hover.png");
             }, function() {
@@ -9,9 +9,13 @@ $(document).ready(function(){
 // ISI Scroll animation
 
 // Elements
-var f1_t1 = document.getElementById("f1_txt1")
+var f1 = document.getElementById("frame1")
+,     f2 = document.getElementById("frame2")
+,     f3 = document.getElementById("frame3")
+,     f4 = document.getElementById("frame4")
+,     f1_t1 = document.getElementById("f1_txt1")
 ,     f1_t2 = document.getElementById("f1_txt2")
-,     f1_i1 = document.getElementById("patient")
+,     f1_i1 = document.getElementById("f1_img1")
 ,     f2_t1 = document.getElementById("f2_txt1")
 ,     f2_i1 = document.getElementById("f2_img1")
 ,     f2_i2 = document.getElementById("heart_sign")
@@ -31,36 +35,54 @@ var f1_t1 = document.getElementById("f1_txt1")
 ;
 
 // Animation
-var t1 = new TimelineLite();
+var tll1 = new TimelineLite();
+// var l1 = 152
+// ,		t1 = "-35"
+var l1 = 132
+,		t1 = "-55"
+,		l2 = 172
+,		t2 = 27
+// ,		l3 = 270
+// ,		t3 = 80
+,		l3 = 250
+,		t3 = 60
+,		l4 = 320
+,		t4 = 120
+,		l0 = 92
+,		t0 = "-95"
+,		o1 = 0.7
+,		o2 = 0.5
+;
 
-t1
-// .from( f1_t1, 0.5, {left: 300, ease:"easeOutExpo"})
-.from( f1_t2, 1, {left: 300, ease:"easeOutExpo"},"0.5")
-.from( f1_i1, 0.5, {scale: 0, ease:"easeOutBack"})
-.to( f2_i1, 1, {scale:0.5,left:120,top:-58, ease:"easeInOutBack"})
+tll1
+.from( f1, 0.5, {autoAlpha: 0, ease:"easeInOutBack"})
+.to( f1_i1, 0.5, {scale:0.6, left:l1, top:t1, ease:Back.easeOut.config(0.8)},"-=0.5")
+.from( f1_t2, 1, {top: "-=20", ease:"easeInOutBack"})
+.to( f1_t2, 1, {opacity:1, ease:"easeInOutBack"},"-=1")
+.to( f1_i1, 1, {scale:1,opacity:1, left:l2, top:t2, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f2_i1, 1, {scale:0.6,opacity:o1,left:l1,top:t1, ease:Back.easeOut.config(0.8)},"-=1")
 .to( f1_t2, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"+=1")
 .from( f2_t1, 1, {top: "-=20", ease:"easeInOutBack"})
 .to( f2_t1, 1, {autoAlpha:1, ease:"easeInOutBack"},"-=1")
-.to( f2_i1, 1, {scale:1,left:175,top:26, ease:"easeInOutBack"},"-=1")
-.to( f1_i1, 1, {scale:0.5,left:270,top:80, ease:"easeInOutBack"},"-=1")
-.to( f3_i1, 1, {scale:0.5,left:120,top:-58, ease:"easeInOutBack"},"-=1")
+.to( f2_i1, 1, {scale:1,opacity:1, left:l2, top:t2, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f1_i1, 1, {scale:0.6,opacity:o1,left:l3,top:t3, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f3_i1, 1, {scale:0.6,opacity:o1,left:l1,top:t1, ease:Back.easeOut.config(0.8)},"-=1")
 .to( f2_t1, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"+=1")
 .to( f1_t1, 1, {top: "30", ease:"easeInOutBack"})
-.from( f3_t1, 1, {top: "-=20", ease:"easeInOutBack"},"-=0.5")
+.from( f3_t1, 1, {top: "-=20", ease:"easeInOutBack"})
 .to( f3_t1, 1, {autoAlpha:1, ease:"easeInOutBack"},"-=1")
-.to( f3_i1, 1, {scale:1,left:175,top:26, ease:"easeInOutBack"},"-=1")
-.to( f2_i1, 1, {scale:0.5,left:247,top:88, ease:"easeInOutBack"},"-=1")
-.to( f1_i1, 1, {left:320,top:120,autoAlpha:0, ease:"easeInOutBack"},"-=1")
-.to( f4_i1, 1, {scale:0.5,left:120,top:-58, ease:"easeInOutBack"},"-=1")
-.to( f4_i1, 1, {opacity:1, ease:"easeInOutBack"},"-=1")
+.to( f3_i1, 1, {scale:1,opacity:1, left:l2,top:t2, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f2_i1, 1, {scale:0.6,opacity:o1,left:l3,top:t3, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f1_i1, 1, {scale:0.4,opacity:o2,left:l4,top:t4, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f4_i1, 1, {scale:0.4,opacity:o2,left:l1,top:t1, ease:Back.easeOut.config(0.8)},"-=1")
 .to( f1_t1, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"+=1")
 .to( f3_t1, 0.5, {top: "-=20",autoAlpha:0, ease:"easeInOutBack"},"-=0.5")
 .to( f4_t1, 1, {opacity:1, ease:"easeInOutBack"})
 .from( f4_t2, 1, {top: "-=20", ease:"easeInOutBack"})
 .to( f4_t2, 1, {opacity:1, ease:"easeInOutBack"},"-=1")
-.to( f4_i1, 1, {scale:1,left:185,top:33, ease:"easeInOutBack"},"-=1")
-.to( f3_i1, 1, {scale:0.5,left:300,top:88,autoAlpha:0, ease:"easeInOutBack"},"-=1")
-.to( f2_i1, 1, {left:300,top:100,autoAlpha:0, ease:"easeInOutBack"},"-=1")
+.to( f4_i1, 1, {scale:1,opacity:1,left:l2,top:t2, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f3_i1, 1, {scale:0.6,opacity:o1,left:l3,top:t3,autoAlpha:0, ease:Back.easeOut.config(0.8)},"-=1")
+.to( f2_i1, 1, {scale:0.4,opacity:o2,left:l4,top:t4,autoAlpha:0, ease:Back.easeOut.config(0.8)},"-=1")
 .to( f4_i1, 1, {rotation:47,top:-7,left:197, ease:"easeInOutBack"},"+=1")
 .to( f5_i1, 1, {autoAlpha:1, ease:"easeInOutBack"},"-=0.5")
 .to( f5_i1, 0.5, {scale:1,top:10,left:10, ease:"easeInOut"})
@@ -71,7 +93,5 @@ t1
 .to( f5_i3, 0.5, {autoAlpha:1})
 .from( f5_i2, 1, {left:300, ease:"easeInOutBack"},"-=2")
 .from( f5_i3, 0.5, {left:300, ease:"easeInOutExpo"},"-=0.5")
-// .from( f5_i3, 0.5, {left:300, ease:Back.easeInOut.config(0.5)},"-=0.5")
 .to(shine, 2, {css: {opacity:1, left:250}, ease:"easeInOutExpo"})
-
 ;
