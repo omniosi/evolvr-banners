8b8ad584e06b1dbb883e01a021bba4047a2e5215
ornette coleman <omniosi@gmail.com>
2016-03-04 16:05:56 -0800

    blue 160 done

--- october-response_blue/160x600_v2/js/scripts.js
+++ october-response_blue/160x600_v2/js/scripts.js
@@ -61,13 +61,14 @@ var      s2 = document.getElementsByClassName('screen2')[0]
 ,       scroller = document.getElementsByClassName('scrollBlock')[0]
 ,       footer = document.getElementsByClassName('footer')[0]
 ,       shine = document.getElementsByClassName('shine')[0]
-,       cta = document.getElementsByClassName('txtbtn2')[0]
+,       cta = document.getElementsByClassName('txtbtn')[0]
 ,       clock1 = document.getElementsByClassName('clock1')[0]
 ,       clock2 = document.getElementsByClassName('clock2')[0]
 ,       clock3 = document.getElementsByClassName('clock3')[0]
 ,       clock4 = document.getElementsByClassName('clock4')[0]
 ,       clock5 = document.getElementsByClassName('clock5')[0]
 ,       clock6 = document.getElementsByClassName('clock6')[0]
+,       clock7 = document.getElementsByClassName('clock7')[0]
 ,       pin1 = document.getElementsByClassName('pin1')[0]
 ,       pin2 = document.getElementsByClassName('pin2')[0]
 ,       pin3 = document.getElementsByClassName('pin3')[0]
@@ -83,17 +84,19 @@ var      s2 = document.getElementsByClassName('screen2')[0]
 var t1 = new TimelineLite();
 t1
 .to(s2, 0, {autoAlpha:1, ease:"easeInOutBack"}, "one")
-.from(s2t1, 1, {top:100, ease:"easeInOutBack"}, "+=0.5")
-.to(s2t2, 1, {css: {opacity:1}, ease:"easeInOutBack"}, "-=0.5")
+.from(s2t1, 1, {top:260, ease:"easeInOutBack"}, "+=0.5")
+.to(s2t2, 1, {css: {opacity:1}, ease:"easeInOutBack"}, "-=1")
 .from(clock1,0.5,{scale:0,ease:"easeInOutBack"})
 .from(clock4,0.5,{scale:0,ease:"easeInOutBack"})
 .from(clock6,0.5,{scale:0,ease:"easeInOutBack"}, "-=0.25")
 .from(clock3,0.5,{scale:0,ease:"easeInOutBack"})
 .from(clock2,0.5,{scale:0,ease:"easeInOutBack"}, "-=0.25")
 .from(clock5,0.5,{scale:0,ease:"easeInOutBack"})
+.from(clock7,0.5,{scale:0,ease:"easeInOutBack"}, "-=0.25")
 .to(s2, 1, {autoAlpha:0, ease:"easeInOutBack"},"cross")
 .to(s3, 1, {autoAlpha:1, ease:"easeInOutBack"},"cross")
-.from(s3t2, 1, {opacity:0, top:108, ease:"easeInOutBack"})
+.to(s2t2, 1, {top:"-=20", ease:"easeInOutBack"},"cross")
+.from(s3t2, 1, {opacity:0, top:"-=20", ease:"easeInOutBack"},"-=0.5")
 .from(pin1,0.5,{top:"-=30",opacity:0,ease:"easeInOutBack"})
 .from(pin2,0.25,{top:"-=30",opacity:0,ease:"easeInOutBack"}, "-=0.25")
 .from(pin3,0.5,{top:"-=30",opacity:0,ease:"easeInOutBack"})
@@ -107,16 +110,15 @@ t1
 .to(s3, 1, {autoAlpha:0, ease:"easeInOutBack"})
 .to(s4, 1, {autoAlpha:1, ease:"easeInOutBack"}, "fourin-=1")
 // .to(s4t2, 1, {css: {opacity:1}, ease:"easeInOutBack"}, "-=0.25")
-.from(s4box, 1, {css: {right:-300}, ease:"easeOutExpo"}, "-=0.25")
+.from(s4box, 0.5, {css: {right:"-=200"}, ease:"easeOutExpo"}, "-=0.25")
 .to(scroller, 1, {autoAlpha:1, ease:"easeInOutBack"})
 .to(footer, 1, {autoAlpha:1, ease:"easeInOutBack"}, "-=1")
-.to(s4box, 1, {opacity:0, ease:"easeOut"}, "+=1")
-.to(s5, 1, {autoAlpha:1, ease:"easeOut"}, "-=1")
+.to(s5, 1, {autoAlpha:1, ease:"easeOut"}, "fourout")
 .to(s4t1, 0.5, {css: {opacity:0}, ease:"easeInOutBack"}, "fourout")
 .to(s4t2, 0.5, {css: {opacity:0}, ease:"easeInOutBack"}, "fourout")
-.to(s5logo, 1, {top:10, ease:"easeInOutExpo"},"-=0.5")
+// .to(s5logo, 1, {top:"-=20", ease:Back.easeInOut},"-=0.5")
 .from(s5t1, 1, {top:"-=20", opacity:0, ease:"easeInOutBack"},"-=0.8")
-.from(cta, 1, {right:-200, ease:"easeInOutBack"}, "cta-=1")
+.from(cta, 1, {right:"-=200", ease:"easeInOutBack"}, "cta-=1")
 .to(cta, 1, {opacity:1, ease:"easeInOutBack"}, "cta-=1")
 .to(shine, 2, {css: {opacity:1, left:150}, ease:"easeInOutExpo"})
 ;
